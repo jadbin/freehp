729d863d7d9b3d65e81740324a26a8bb875987fb
jadbin <jadbin.com@hotmail.com>
2018-06-12 22:28:43 +0800

    fix anonymity check

--- freehp/checker.py
+++ freehp/checker.py
@@ -5,17 +5,29 @@ import random
 import asyncio
 import logging
 from asyncio import CancelledError
+import re
 
 import aiohttp
 import async_timeout
 
 log = logging.getLogger(__name__)
 
+IP_REG = re.compile('^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$')
+
+HTTP_CHECK_URL = 'http://httpbin.org/get'
+
 
 class HttpbinChecker:
     def __init__(self, *, loop=None, timeout=10):
-        self._loop = loop or asyncio.get_event_loop()
-        self._timeout = int(timeout)
+        self.loop = loop or asyncio.get_event_loop()
+        self.timeout = int(timeout)
+
+        try:
+            self.origin_ip = self.loop.run_until_complete(self.get_origin_ip())
+        except Exception:
+            log.error('Failed to get origin IP address', exc_info=True)
+            raise
+        log.info('Origin IP address: %s', self.origin_ip)
 
     @classmethod
     def from_manager(cls, manager):
@@ -29,24 +41,41 @@ class HttpbinChecker:
         else:
             proxy = addr
         try:
-            async with aiohttp.ClientSession(loop=self._loop) as session:
-                with async_timeout.timeout(self._timeout, loop=self._loop):
+            async with aiohttp.ClientSession(loop=self.loop) as session:
+                with async_timeout.timeout(self.timeout, loop=self.loop):
                     seed = str(random.randint(0, 99999999))
-                    url = "http://httpbin.org/get?seed={}".format(seed)
+                    url = "{}?show_env=1&seed={}".format(HTTP_CHECK_URL, seed)
                     async with session.request("GET", url, proxy=proxy, headers={'Connection': 'keep-alive'}) as resp:
                         body = await resp.read()
-                        data = json.loads(body.decode('utf-8'))
+                        data = json.loads(body.decode())
                         if data['args'].get('seed') != seed:
                             return False
-                        # remove transparent proxy
-                        if ',' in data['origin']:
+                        if self.origin_ip not in data['origin']:
                             anonymity = 1
-                        if 'Via' not in data['headers'] and 'Proxy-Connection' not in data['headers']:
+                        if self._is_elite_proxy(data):
                             anonymity = 2
-
         except CancelledError:
             raise
         except Exception:
             return False
         log.debug("Proxy %s is OK", addr)
         return True, anonymity
+
+    async def get_origin_ip(self):
+        async with aiohttp.ClientSession(loop=self.loop) as session:
+            with async_timeout.timeout(self.timeout, loop=self.loop):
+                async with session.request('GET', HTTP_CHECK_URL) as resp:
+                    body = await resp.read()
+                    data = json.loads(body.decode())
+                    ip = data['origin']
+        assert IP_REG.match(ip) is not None
+        return ip
+
+    def _is_elite_proxy(self, data):
+        if ',' in data['origin']:
+            return False
+        if 'Proxy-Connection' in data['headers']:
+            return False
+        if ',' in data['headers']['Via']:
+            return False
+        return True
